051d27a241785834c781c712ae9d08e799828d4f
TheTachyonProject <jordankettner0@gmail.com>
2026-09-06 22:56:03 -0400

    Wire live Whop checkout links

---
+++ checkout-config.js
@@ -0,0 +1,6 @@
+window.TACHYON_CHECKOUTS = {
+  monthly: 'https://whop.com/checkout/plan_DulX0ELfxzSyT',
+  quarterly: 'https://whop.com/checkout/plan_73Ny0h9vbhLlp',
+  annual: 'https://whop.com/checkout/plan_7NQhyigSMh7iu',
+  lifetime: 'https://whop.com/checkout/plan_3AurEJr6b252f'
+};
